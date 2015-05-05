051faa342ba65b6f9e77c66d8ef90e7204b95dbc
Captain-Eli <eli@nonap.com>
2015-05-05 03:19:13 +0100

    Create Enemies.py

---
+++ Enemies.py
@@ -0,0 +1,23 @@
+class Enemy:
+	def	__init__(self, name, hp, damage):
+		self.name = name
+		self.hp = hp
+		self.damage = damage
+	def is_alive(self):
+		return self.hp > 0
+
+class SpookySkeleton(Enemy):
+	def __init__(self):
+		super().__init__(name="Spooky Skeleton", hp=15, damage=3)
+
+class LackyJoblin(Enemy):
+	def __init__(self):
+		super().__init__(name="Lacky Skeleton", hp=15, damage=2)
+
+class StupidAnimal(Enemy):
+	def __init__(self):
+		super().__init__(name="Stupid Animal", hp=5, damage=1)
+
+class MoistSlime(Enemy):
+	def __init__(self):
+		super().__init__(name="Moist Slime", hp=10, damage=1)
